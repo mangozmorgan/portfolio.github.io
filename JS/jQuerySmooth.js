f25eda0aab61edf5766287d17a860ffe60c9145c
mangozmorgan <morganpsyinfo@gmail.com>
2021-10-29 10:25:14 +0200

    up ankor

--- JS/jQuerySmooth.js
+++ JS/jQuerySmooth.js
@@ -13,3 +13,10 @@ $("#timeIcon").on('click', function (event) {
         scrollTop: $($.attr(this, 'href')).offset().top
     }, 500);
 });
+$("#dedIcon").on('click', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+});
